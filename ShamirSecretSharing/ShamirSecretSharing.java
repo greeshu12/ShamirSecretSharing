d17a95acb54bbeb3bacc9ac03ed7761a0806ac3f
greeshma chowdary <146710357+greeshu12@users.noreply.github.com>
2024-09-30 12:52:14 +0530

    ShamirSecretSharing.java

--- ShamirSecretSharing/ShamirSecretSharing.java
+++ ShamirSecretSharing/ShamirSecretSharing.java
@@ -1,60 +1,62 @@
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 import org.json.JSONObject;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class ShamirSecretSharing {
+   public ShamirSecretSharing() {
+   }
 
-    public static void main(String[] args) {
-        String jsonInput = "{ \"keys\": { \"n\": 4, \"k\": 3 }, \"1\": { \"base\": \"10\", \"value\": \"4\" }, \"2\": { \"base\": \"2\", \"value\": \"111\" }, \"3\": { \"base\": \"10\", \"value\": \"12\" }, \"6\": { \"base\": \"4\", \"value\": \"213\" } }";
+   public static void main(String[] var0) {
+      String var1 = "{ \"keys\": { \"n\": 4, \"k\": 3 }, \"1\": { \"base\": \"10\", \"value\": \"4\" }, \"2\": { \"base\": \"2\", \"value\": \"111\" }, \"3\": { \"base\": \"10\", \"value\": \"12\" }, \"6\": { \"base\": \"4\", \"value\": \"213\" } }";
+      JSONObject var2 = new JSONObject(var1);
+      int var3 = var2.getJSONObject("keys").getInt("n");
+      int var4 = var2.getJSONObject("keys").getInt("k");
+      HashMap var5 = new HashMap();
+      Iterator var6 = var2.keySet().iterator();
 
-        JSONObject jsonObject = new JSONObject(jsonInput);
-        int n = jsonObject.getJSONObject("keys").getInt("n");
-        int k = jsonObject.getJSONObject("keys").getInt("k");
+      while(var6.hasNext()) {
+         String var7 = (String)var6.next();
+         if (!var7.equals("keys")) {
+            int var8 = Integer.parseInt(var2.getJSONObject(var7).getString("base"));
+            int var9 = Integer.parseInt(var2.getJSONObject(var7).getString("value"));
+            var5.put(var8, var9);
+         }
+      }
 
-        // Collect points from the JSON
-        Map<Integer, Integer> points = new HashMap<>();
-        for (String key : jsonObject.keySet()) {
-            if (!key.equals("keys")) {
-                int base = Integer.parseInt(jsonObject.getJSONObject(key).getString("base"));
-                int value = Integer.parseInt(jsonObject.getJSONObject(key).getString("value"));
-                points.put(base, value);
+      int var10 = calculateConstantTerm(var5, var4);
+      System.out.println("The constant term c of the polynomial is: " + var10);
+   }
+
+   private static int calculateConstantTerm(Map<Integer, Integer> var0, int var1) {
+      int[] var2 = new int[var1];
+      int[] var3 = new int[var1];
+      int var4 = 0;
+
+      for(Iterator var5 = var0.entrySet().iterator(); var5.hasNext(); ++var4) {
+         Map.Entry var6 = (Map.Entry)var5.next();
+         if (var4 >= var1) {
+            break;
+         }
+
+         var2[var4] = (Integer)var6.getKey();
+         var3[var4] = (Integer)var6.getValue();
+      }
+
+      int var9 = 0;
+
+      for(int var10 = 0; var10 < var1; ++var10) {
+         int var7 = var3[var10];
+
+         for(int var8 = 0; var8 < var1; ++var8) {
+            if (var10 != var8) {
+               var7 *= (0 - var2[var8]) / (var2[var10] - var2[var8]);
             }
-        }
+         }
 
-        // Calculate the constant term c of the polynomial
-        int c = calculateConstantTerm(points, k);
-        System.out.println("The constant term c of the polynomial is: " + c);
-    }
+         var9 += var7;
+      }
 
-    private static int calculateConstantTerm(Map<Integer, Integer> points, int k) {
-        int[] x = new int[k];
-        int[] y = new int[k];
-        int index = 0;
-
-        // Extract the first k points
-        for (Map.Entry<Integer, Integer> entry : points.entrySet()) {
-            if (index < k) {
-                x[index] = entry.getKey();
-                y[index] = entry.getValue();
-                index++;
-            } else {
-                break;
-            }
-        }
-
-        // Calculate the constant term using Lagrange interpolation
-        int c = 0;
-        for (int i = 0; i < k; i++) {
-            int term = y[i];
-            for (int j = 0; j < k; j++) {
-                if (i != j) {
-                    term *= (0 - x[j]) / (x[i] - x[j]); // x=0 for calculating c
-                }
-            }
-            c += term;
-        }
-
-        return c;
-    }
+      return var9;
+   }
 }
